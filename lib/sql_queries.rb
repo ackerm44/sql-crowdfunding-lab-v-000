a6df6fcf0a77aa42280a6956089941304a77545c
Amanda Ackerman <ackerm44@gmail.com>
2017-11-27 22:11:24 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   #difference = project.title
-  
+
 "difference = SUM(pledges.amount) - projects.funding_goal
 
 SELECT projects.title, difference
